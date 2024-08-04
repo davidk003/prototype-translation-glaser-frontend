c4f353d9ed4a62598fe4a549d7ebb2b15a102526
David <davidkimgba@gmail.com>
2024-08-04 15:27:44 +0900

    chore: Update sidebar component with menu items and icons

--- src/lib/components/custom-ui/sidebar/sidebar.svelte
+++ src/lib/components/custom-ui/sidebar/sidebar.svelte
@@ -1,18 +1,37 @@
 <script lang="ts">
     import * as Sheet from "$lib/components/ui/sheet";
+    import { HomeIcon, ChartLine, Settings, Settings2 } from "lucide-svelte";    
 </script>
 
 <div>
     <Sheet.Root>
-        <Sheet.Trigger>Open</Sheet.Trigger>
+        <Sheet.Trigger id="trigger">Open</Sheet.Trigger>
         <Sheet.Content side="left">
             <Sheet.Header>
-            <Sheet.Title>Are you sure absolutely sure?</Sheet.Title>
-            <Sheet.Description>
-                This action cannot be undone. This will permanently delete your account
-                and remove your data from our servers.
-            </Sheet.Description>
-            </Sheet.Header>
+                <Sheet.Title class="gap-3">Menu</Sheet.Title>
+                <!-- <Sheet.Description>
+                    This action cannot be undone. This will permanently delete your account
+                    and remove your data from our servers.
+                </Sheet.Description> -->
+                </Sheet.Header>
+
+            <nav class="flex flex-col gap-4">
+                <a href="/" class="flex items-center gap-3 hover:text-primary">
+                    <HomeIcon/>
+                    Home
+                </a>
+                <a on:click={()=>{document.getElementById("trigger")?.click()}} href="/dashboard/analytics" class="flex items-center gap-3 hover:text-primary">
+                    <!-- <svg data-id="3" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-5 w-5"><path d="M22 12h-2.48a2 2 0 0 0-1.93 1.46l-2.35 8.36a.25.25 0 0 1-.48 0L9.24 2.18a.25.25 0 0 0-.48 0l-2.35 8.36A2 2 0 0 1 4.49 12H2"></path></svg> -->
+                    <ChartLine/>
+                    Analytics
+                </a>
+                <a href="/" class="flex items-center gap-3 hover:text-primary">
+                    <Settings/>
+                    <!-- <Settings2/> -->
+                    Settings
+
+                </a>
+            </nav>
         </Sheet.Content>
     </Sheet.Root>
 </div>
