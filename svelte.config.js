a3ad945475eb9107e89d9025081805a96ddf2604
David <davidkimgba@gmail.com>
2024-08-23 17:13:52 -0700

    Switched adapter from auto to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
+// /** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/adapter-vercel').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
